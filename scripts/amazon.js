8b4191213e979f2ce88e38fe1876736d5d9699ed
Shina <m.atete@alustudent.com>
2025-03-14 11:46:26 +0200

    New changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,13 +46,35 @@ products.forEach((product) => {
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-name="${product.name}">
                 Add to Cart
             </button>
         </div>
     `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productName = button.dataset.productName;
+
+            let matchingItem;
+
+            cart.forEach((item) => {
+                if (productName === item.productName) {
+                    matchingItem = item;
+                }
+            });
+            if (matchingItem) {
+                matchingItem.quantity += 1;
+            } else {
+                cart.push({
+                    productName: productName,
+                    quantity: 1
+                });
+            }     
+        });
+    });
