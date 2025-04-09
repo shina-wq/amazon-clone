ceb649152007614461aeb89de5204fd6dc96b9ca
Shina <m.atete@alustudent.com>
2025-04-09 15:38:37 +0200

    Delete from cart feature

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-import { products } from "./products.js";
+import {products} from "./products.js";
 
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2, 
 }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
@@ -25,3 +25,15 @@ export function addToCart(productId) {
         });
     }
 };
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+}
